cc80ae2099395d0be20b6e07578aa9b7e59823da
Valentin Chmara <valentinchmara@gmail.com>
2024-04-18 00:25:33 +0200

    fix: import file

--- contracts/src/mock/MockVRFCoordinator.sol
+++ contracts/src/mock/MockVRFCoordinator.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-// A mock for testing code that relies on VRFCoordinatorV2.
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.23;
 
 import {VRFCoordinatorV2Interface} from "../VRFCoordinatorV2Interface.sol";
 import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
