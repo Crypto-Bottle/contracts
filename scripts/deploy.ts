4941075ed9f494c660d0559d29f124119d98e422
Valentin Chmara <valentinchmara@gmail.com>
2024-05-21 10:42:48 +0200

    feat: deploy implementation V1

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,5 @@
 import { ethers, upgrades } from "hardhat";
-import { Contract } from "ethers";
+import { IVRFCoordinatorV2Plus } from "../typechain-types";
 
 // Deploy ONLY for polygon amoy
 const polygonUSDC = "0x41E94Eb019C0762f9Bfcf9Fb1E58725BfB0e7582";
@@ -8,11 +8,10 @@ const coordinator = "0x343300b5d84D444B2ADc9116FEF1bED02BE49Cf2";
 const vrfCoordinatorAddress = "0x343300b5d84d444b2adc9116fef1bed02be49cf2";
 
 async function createChainlinkSubscription(
-  vrfCoordinator: Contract,
+  vrfCoordinator: IVRFCoordinatorV2Plus,
 ): Promise<string> {
   const tx = await vrfCoordinator.createSubscription();
   const receipt = await tx.wait();
-  console.dir(receipt?.logs);
   const subscriptionId = BigInt(receipt?.logs?.[0]?.topics?.[1] || 0);
   if (!subscriptionId) {
     throw new Error("Subscription ID not found");
@@ -25,7 +24,7 @@ async function main() {
 
   const [deployer] = await ethers.getSigners();
   const vrfCoordinator = await ethers.getContractAt(
-    "VRFCoordinatorV2Interface",
+    "IVRFCoordinatorV2Plus",
     vrfCoordinatorAddress,
     deployer,
   );
@@ -37,31 +36,21 @@ async function main() {
   const CryptoCuvee = await ethers.getContractFactory("CryptoCuvee");
 
   console.log("Deploying the CryptoCuvee contract...");
-  const cryptoCuvee = await upgrades.deployProxy(CryptoCuvee, {
-    initializer: false,
-  });
-
-  await cryptoCuvee.waitForDeployment();
-  console.log(
-    "CryptoCuvee Implementation deployed to:",
-    await cryptoCuvee.getAddress(),
+  const cryptoCuvee = await upgrades.deployProxy(CryptoCuvee,
+    [
+      polygonUSDC,
+      [],
+      "https://app.cryptobottle.fr/",
+      systemWallet,
+      coordinator,
+      ethers.keccak256(ethers.toUtf8Bytes("CryptoBottleHash")),
+      2000000,
+      1,
+      subId,
+    ],
+    { initializer: "initialize" },
   );
-
-  console.log("Initializing the CryptoCuvee contract...");
-  const tx = await cryptoCuvee.initialize(
-    polygonUSDC,
-    [],
-    "https://app.cryptobottle.fr/",
-    systemWallet,
-    coordinator,
-    ethers.keccak256(ethers.toUtf8Bytes("CryptoBottleHash")),
-    2000000,
-    1,
-    subId,
-  );
-
-  await tx.wait();
-  console.log("CryptoCuvee contract initialized successfully");
+  console.log(`CryptoCuvee deployed to: ${await cryptoCuvee.getAddress()}`);
 }
 
 main().catch((error) => {
